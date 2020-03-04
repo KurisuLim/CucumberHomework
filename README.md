fdfa55d01e2beae3e85627287abe395b18ec718f
KurisuLim <kazuki1002003@yahoo.com>
2020-03-03 22:08:28 -0800

    updates the readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CucumberHomework
+### Homework-Implement Gherkin to your current tests
+#### JAVA AUTOMATION A4  Homework-implement Gherkin to your current tests
+1. Implement Gherkin to your current tests
+2. Refactor your framework to use cucumber.
+Write your tests in cucumber language. Map them with translation in Java
+
+### Homework-Implement Cucumber Tables for BDD
+#### JAVA AUTOMATION A4  Homework-Implement Cucumber Tables for BDD
+1. Implement Cucumber Tables for BDD
+2. Use Cucumber tables for Data-Driven testing.
+refactor your tests to use tests data from those tables
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,22 +1,22 @@
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(
-//        features = "src/test/resources/features/Login.feature",
-//        glue = {"stepdefs"},
-//        tags = {"~@Ignore"},
-//        dryRun = false,
-//        strict = true,
-//        monochrome = true,
-//        plugin = {
-//                "pretty",
-//                "html:target/cucumber-reports/cucumber-pretty",
-//                "json:target/cucumber-reports/CucumberTestReport.json",
-//                "rerun:target/cucumber-reports/rerun.txt"
-//        }
-//        )
-//
-//public class TestRunner {
-//}
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/Login.feature",
+        glue = {"stepdefs"},
+        tags = {"~@Ignore"},
+        dryRun = false,
+        strict = true,
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        }
+        )
+
+public class TestRunner {
+}
